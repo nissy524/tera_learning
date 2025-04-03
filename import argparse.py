e9bea1954e5b4b24ca7eaba81b3fa11e77893457
nissy524 <kouseigaluin.like@icloud.com>
2025-04-03 23:01:01 +0900

    まじでbashてなに

---
+++ import argparse.py
@@ -0,0 +1,11 @@
+import argparse
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('numbers',
+                        help='調べたい数値を入れてください',
+                        type=int,
+                        nargs=3)
+    args=parser.parse_args()
+    print(args.numbers)
+    #bashの使い方不明

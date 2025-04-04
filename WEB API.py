663ef2217fb1e6fb2c5ef7a88a8461f1327fdb69
nissy524 <kouseigaluin.like@icloud.com>
2025-04-04 23:57:52 +0900

    APTAPTAPT

---
+++ WEB API.py
@@ -0,0 +1,25 @@
+import requests
+res=requests.get('https://zipcloud.ibsnet.co.jp/api/search',params={'zipcode':'2110041'})
+# search?zipcode=2110041と同じ使い方　=params
+print(res.status_code)
+print(res.text)
+
+import requests
+res=requests.get('https://zipcloud.ibsnet.co.jp/api/search?zipcode=2110041')
+res_json=res.json()
+results=res_json['results'][0]
+address=results['address1']+results['address2']+results['address3']
+print(address)
+
+#APIによって申請を送らないといけないのもある。例は下記
+import requests
+res=requests.get('https://zipcloud.ibsnet.co.jp/api/search',
+                  headers={'Authorization':'xxxxx'})
+# xxxxxは取得したキーのこと？
+
+
+
+
+
+
+#res.status_code:200=ok,400=bad request,500=server error

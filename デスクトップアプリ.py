1571d7cf613f99fe74726e76339df184e0e7d995
nissy524 <kouseigaluin.like@icloud.com>
2025-04-05 21:01:48 +0900

    tkinterのほうが良い　pysimplegui

--- デスクトップアプリ.py
+++ デスクトップアプリ.py
@@ -3,6 +3,7 @@ import PySimpleGUI as sg
 layout= [[sg.Text('名前: '),sg.InputText(key='-NAME-')],
          [sg.Text('住所: '),sg.InputText(key='-ADDRESS-')],
          [sg.Button('実行',key='-SUBMIT-')]]
+#なぞにtextが読み込まない
 
 window = sg.Window('てらんぼ',layout,size=(300,150))
 

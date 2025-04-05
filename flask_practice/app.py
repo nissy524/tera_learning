ad1752ebcb220eba3679d759a9dd682f5b2d87c5
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2025-04-06 01:20:48 +0900

    add: flask practice

---
+++ flask_practice/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return render_template('index.html') # templatesフォルダ内のindex.htmlを表示する
+
+@app.route('/info')
+def info():
+    data = {
+        "name": "terashita",
+        "age": 25
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
